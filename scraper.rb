1860a6941e157d8ab4d5fd6ba0c8b04e75a32f2e
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-08 12:39:52 -0500

    add scraping the name and brand

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,15 @@
 require 'httparty'
+require 'nokogiri'
 
 response = HTTParty.get('https://www.houseoffraser.co.uk/men/hoodies-and-sweatshirts', {
   headers: {"User-Agent" => "Httparty"},
-  debug_output: STDOUT, # To show that User-Agent is Httparty
+  # debug_output: STDOUT, # To show that User-Agent is Httparty
 })
-puts response.body
+
+document = Nokogiri::HTML4(response.body)
+items = document.css('div.s-productthumbbox')
+items.each { |div|
+  name = div.css('span.productdescriptionname').text
+  brand = div.css('span.productdescriptionbrand').text
+  price = div.css()
+}
